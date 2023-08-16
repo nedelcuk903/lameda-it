ecc9156d9e8035f107dd6c89bd064774183b811a
VGcod3 <68184912+VGcod3@users.noreply.github.com>
2023-08-16 11:44:52 +0300

    new form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -4,7 +4,7 @@ export default function ContactForm() {
             <h2 className="text-center text-cyan-500 text-5xl font-bold mb-10">Форма обратной связи</h2>
 
             <div className="flex flex-col justify-center align-middle">
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScgEnDDAilr1rDhCdgPAgtbZcWNXGv1DDpBlBBKv48opV6VLg/viewform?embedded=true" width="100%" height="1300" >Загрузка…</iframe>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdQzEKvopXd5_-dWKJizj2Mzhpm9E0atJA9XQvBNBxAlYxH0g/viewform?embedded=true" width="100%" height="1300" >Загрузка…</iframe>
             </div>
         </div>
     );
